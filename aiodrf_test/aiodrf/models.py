a257522c24f8c6c592b238d8ea7f60d8959fc1fa
Maria Hl <imgvoid@gmail.com>
2023-10-03 17:50:33 +0300

    delete test direct connection model

--- aiodrf_test/aiodrf/models.py
+++ aiodrf_test/aiodrf/models.py
@@ -40,7 +40,7 @@ class TestIncluded(models.Model):
     def __str__(self):
         return self.text_included
 
-# TODO: create the MultipleChoiceField from the module or the ArrayField
+
 class Test(models.Model):
     text = models.CharField(max_length=128, default='')
     int = models.IntegerField(default=1)
@@ -51,24 +51,6 @@ class Test(models.Model):
     ), default='UK')
     foreign_key = models.ForeignKey(TestIncluded, on_delete=models.SET_NULL, null=True, blank=True, related_name='test_included')
     many_to_many = models.ManyToManyField(TestIncluded, blank=True, related_name='test_included_many')
-    class Meta:
-        verbose_name = _('Test')
-        verbose_name_plural = _('Tests')
-        default_related_name = 'test'
-    
-    def __str__(self):
-        return self.text
-
-
-class TestDirectCon(models.Model):
-    text = models.CharField(max_length=128, default='')
-    int = models.IntegerField(default=1)
-    bool = models.BooleanField(default=False)
-    choice_int = models.IntegerField(choices=((1, 'One'), (2, 'Two')), default=1)
-    choice_str = models.CharField(max_length=9, choices=(
-        ('UK', 'United Kingdom'), ('US', 'United States')
-    ), default='UK')
-    foreign_key = models.ForeignKey(TestIncluded, on_delete=models.SET_NULL, null=True, blank=True, related_name='test_dir_test_included')
     class Meta:
         verbose_name = _('Test')
         verbose_name_plural = _('Tests')
